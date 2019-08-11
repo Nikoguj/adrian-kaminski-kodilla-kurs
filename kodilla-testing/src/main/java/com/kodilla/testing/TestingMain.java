005d6dd9090f34dde1428fdb1a60852a517a1d60
Nikoguj <Nikoguj@gmail.com>
2019-08-11 21:01:54 +0200

    First Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -3,6 +3,37 @@ package com.kodilla.testing;
 public class TestingMain {
     public static void main(String[] args)
     {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if(result.equals("theForumUser"))
+        {
+            System.out.println("Test OK");
+        }else
+        {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        Calculator calculator = new Calculator();
+
+        if(calculator.add(1 , 2) == 3)
+        {
+            System.out.println("Test OK");
+        }else
+        {
+            System.out.println("Error");
+        }
+
+        if(calculator.subtract(4,2) == 2)
+        {
+            System.out.println("Test OK");
+        }else
+        {
+            System.out.println("Error");
+        }
+
     }
 }

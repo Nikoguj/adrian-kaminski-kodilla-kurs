1820939f2d0038faf6bb51e1c11c1fa14e1459da
Nikoguj <Nikoguj@gmail.com>
2019-08-16 14:08:56 +0200

    First Commit

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,31 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        /*ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);*/
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        String text1 = "lambda is shorter";
+        poemBeautifier.beautify(text1, text -> "ABC" + text1 + "ABC");
+        poemBeautifier.beautify(text1, text -> text1.toUpperCase());
+        poemBeautifier.beautify(text1, text -> text1.substring(0, 6));
+        poemBeautifier.beautify(text1, text -> text1.replace("l", "L"));
+    }
+}

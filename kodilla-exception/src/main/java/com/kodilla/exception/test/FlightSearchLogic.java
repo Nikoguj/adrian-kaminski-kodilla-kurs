ffbff509147dc2e26f5128ffaf60c6da24754552
Nikoguj <Nikoguj@gmail.com>
2019-08-22 14:36:03 +0200

    8.4

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearchLogic.java
@@ -0,0 +1,33 @@
+package com.kodilla.exception.test;
+
+import javax.management.relation.RoleInfoNotFoundException;
+import java.util.HashMap;
+
+public class FlightSearchLogic {
+    public boolean findFlight(Flight flight) throws RoleInfoNotFoundException {
+        HashMap<String, Boolean> airports = new HashMap<>();
+        airports.put("Międzynarodowy Port Lotniczy Atlanta", true);
+        airports.put("Międzynarodowy Stołeczny Port Lotniczy Pekin", false);
+        airports.put("Międzynarodowy Port Lotniczy Dubaj", true);
+        airports.put("Port Lotniczy Haneda", true);
+        airports.put("Międzynarodowy Port Lotniczy Los Angeles", false);
+        airports.put("Międzynarodowy Port Lotniczy Chicago-O’Hare", false);
+        airports.put("Port lotniczy Londyn-Heathrow", true);
+        airports.put("Międzynarodowy Port Lotniczy Hongkong", false);
+        airports.put("Międzynarodowy Port Lotniczy Szanghaj-Pudong", false);
+
+        if(airports.get(flight.departureAirport) != null && airports.get(flight.arrivalAirport) != null)
+        {
+            if(airports.get(flight.arrivalAirport) == true)
+            {
+                return true;
+            }else
+            {
+                return false;
+            }
+        }else
+        {
+            throw new RoleInfoNotFoundException();
+        }
+    }
+}
---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearchRunner.java
@@ -0,0 +1,31 @@
+package com.kodilla.exception.test;
+
+import javax.management.relation.RoleInfoNotFoundException;
+
+public class FlightSearchRunner {
+    public static void main(String args[]) throws RoleInfoNotFoundException {
+        FlightSearchLogic flightSearchLogic = new FlightSearchLogic();
+        Flight flight1 = new Flight("Międzynarodowy Port Lotniczy Atlanta", "Port lotniczy Londyn-Heathrow");
+
+        if(flightSearchLogic.findFlight(flight1))
+        {
+            System.out.println("flight found");
+        }else{
+            System.out.println("Cannot find flight");
+        }
+
+        Flight flight2 = new Flight("Międzynarodowy Port Lotniczy Atlanta", "Port Lotniczy Frankfurt");
+
+        try {
+            if(flightSearchLogic.findFlight(flight2))
+            {
+                System.out.println("Flight found.");
+            }else{
+                System.out.println("Cannot find flight.");
+            }
+        }catch (Exception e)
+        {
+            System.out.println("Cannot find airport.");
+        }
+    }
+}

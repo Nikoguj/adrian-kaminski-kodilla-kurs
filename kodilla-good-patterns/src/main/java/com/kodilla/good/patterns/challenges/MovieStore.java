470c6118fa8bda1699b1f08d3e5513bc1f79efbd
Nikoguj <Nikoguj@gmail.com>
2019-10-05 16:16:18 +0200

    13.1

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
@@ -1,10 +1,9 @@
-package challenges;
+package com.kodilla.good.patterns.challenges;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
 
 public class MovieStore {
 
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/ShowMovies.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/ShowMovies.java
@@ -1,7 +1,5 @@
-package challenges;
+package com.kodilla.good.patterns.challenges;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 public class ShowMovies {

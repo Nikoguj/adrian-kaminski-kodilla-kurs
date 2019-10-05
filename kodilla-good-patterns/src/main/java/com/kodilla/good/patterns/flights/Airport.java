8a8747ae07519284174720754b4d6b61d67646d0
Nikoguj <Nikoguj@gmail.com>
2019-10-05 16:20:29 +0200

    14.1

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/flights/Airport.java
+++
@@ -1,9 +0,0 @@
-package com.kodilla.good.patterns.flights;
-
-import java.util.List;
-
-public interface Airport {
-    String getAirportName();
-    List<Airport> getFlightTo();
-    void addListFlightTo (List<Airport> flightTo);
-}

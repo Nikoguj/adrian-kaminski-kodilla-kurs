e349d482a1dbfcd5ce7b46813e9719cd813ca2d3
Nikoguj <Nikoguj@gmail.com>
2019-10-05 13:29:04 +0200

    13.1

---
+++ kodilla-good-patterns/src/main/java/challenges/ShowMovies.java
@@ -0,0 +1,16 @@
+package challenges;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ShowMovies {
+    public static void main(String[] args) {
+        MovieStore.getMovies().entrySet()
+                .stream()
+                .map(Map.Entry::getValue)
+                .flatMap(x -> x.stream())
+                .map(x -> x.concat("! "))
+                .forEach(System.out::print);
+    }
+}
